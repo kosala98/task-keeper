447750b9bebbfb2856a84b71f60bb20301d1d7a8
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2023-11-09 13:32:06 +0530

    add props and notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../notes";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Note = () => {
+const Note = (props) => {
   return (
     <div className="note">
-      <h1>This is title</h1>
-      <p>This is content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 };
