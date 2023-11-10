da02ee2f97adb9e822609c0b6f233c21936a20e3
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2023-11-11 00:19:39 +0530

    add final ui designs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
 
 const Header = () => {
   return (
     <header>
-      <h1>Task Keeper</h1>
+      <h1>
+        <NoteAddIcon />
+        Task Keeper
+      </h1>
     </header>
   );
 };
